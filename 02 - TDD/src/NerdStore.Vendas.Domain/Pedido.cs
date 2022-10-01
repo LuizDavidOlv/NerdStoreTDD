cac61cacd8b60d6d6f20a90309a39a4578600bc2
Luiz David <luizdavid.93@hotmail.com>
2022-09-30 21:55:31 -0300

    Order - AddItem - part 1

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            this.pedidoItens = new List<PedidoItem>();
+        }
+        
+        public decimal TotalValue { get; private set; }
+        
+        private readonly List<PedidoItem> pedidoItens;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => this.pedidoItens;
+
+        public void AddItem(PedidoItem pedidoItems)
+        {
+            this.pedidoItens.Add(pedidoItems);
+            TotalValue = PedidoItens.Sum(p => p.UnitValue * p.Ammount);
+        }   
+    }
+}
